a0a34a3277703cabd728c6b0a7ec65bcbc85db18
Manish Bansal <manishbansal8843@gmail.com>
2018-06-26 22:19:32 +0530

    Added error message for devices which are not paired with the mi band

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild,ElementRef,Renderer2 } from '@angular/core';
 import { PedoMeterResult } from 'src/app/PedoMeterResult';
 import { Observable } from 'rxjs/Observable';
 import { MiBandService2Service } from 'src/app/mi-band-service-2.service';
@@ -16,9 +16,13 @@ import { AuthGaurdService } from 'src/app/auth-gaurd.service';
 })
 export class AppComponent implements OnInit,OnDestroy{
   title = 'Mi Smart band AR experience';
-  PairDevice:string='Pair Device';
+  @ViewChild('PairingButton') pairingButton:ElementRef;
+  PairDeviceButtonLabel:string='Pair Device';
+  disablePairingButton:boolean=false;
   //pedoStats:PedoMeterResult;
   miBandService:MiBandService2Service;
+  errorDuringPairing:boolean=false;
+  pairingErrorMssg:string;
   bleNotSupported:boolean;
   ifPaired:boolean=false;
   pairingStarted:boolean=false;
@@ -26,7 +30,7 @@ export class AppComponent implements OnInit,OnDestroy{
   //StepsSubscription:Subscription;
   hideAllButARDisplay:boolean=false;
 
-  constructor(private injector: Injector,private router:Router,private authgaurd:AuthGaurdService){
+  constructor(private injector: Injector,private router:Router,private authgaurd:AuthGaurdService,private renderer:Renderer2){
     if (!navigator.bluetooth) {
       this.bleNotSupported=true;
     }
@@ -57,12 +61,24 @@ export class AppComponent implements OnInit,OnDestroy{
   }*/
 
   doPairing(){
-    this.PairDevice='Pairing..Please Wait.';
+    this.PairDeviceButtonLabel='Pairing..Please Wait.';
+    //this.renderer.setProperty(this.pairingButton.nativeElement,'disabled','true');
+    this.disablePairingButton=true;
+    this.renderer.setStyle(this.pairingButton.nativeElement,'background-color','grey');
+    this.errorDuringPairing=false;
     this.pairingStarted=true;
     if(this.miBandService){
      this.miBandService.getPedoStats().subscribe(()=>{
       this.ifPaired=true;
       this.authgaurd.childRoutesEnabled=true;
+     },error=>{
+      this.pairingStarted=false;
+      this.PairDeviceButtonLabel='Pair Device';
+      this.renderer.setStyle(this.pairingButton.nativeElement,'background-color','green');
+     // this.renderer.setProperty(this.pairingButton.nativeElement,'disabled','false');
+     this.disablePairingButton=false;
+      this.errorDuringPairing=true;
+      this.pairingErrorMssg=error;
      });
   }
 }
